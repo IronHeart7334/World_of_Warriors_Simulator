2e0a0b03b6de571aa95500bf4f804446198d2ebe
IronHeart7334 <greengrappler12@gmail.com>
2016-08-25 15:54:06 -0700

    Updated warrior file

--- warrior_file.js
+++ warrior_file.js
@@ -5,59 +5,68 @@ var water = new Element("Water", "rgb(0, 0, 255)", "Air");
 var no_ele = new Element("Null", "rgb(100, 100, 100)", undefined);
 
 
-// this will need to be changed in the future			
-var Abu = new Warrior(["Abu", [0.9334, 0.4, 1.05, 1], fire, new Ele_beat(1.7, 1, "f")], 34);
-var Aka = new Warrior(["Aka", [1, 0.4, 0.96, 0], earth, new Ele_beat(2.2, 2, "e")], 34);
-var Lance = new Warrior(["Lance", [0.7334, 0.1, 1.03, 2], earth, new Ele_beat(1.72, 1, undefined)], 34);
-var Erika = new Warrior(["Erika", [1.0334, 0.4, 0.95, 1], air, new AOE(1.5, 2, "a")], 34);
 
-// data
-var bart_data = ["My name are BART", [8.999, 0.000001, 8.2, 8], water, new Ele_beat(32, 8, "f")];
+var classic_red_data = ["No name set", [1.057, 0.4, 0.95, 1], fire, new Ele_beat(1.66, 1, "f"), new Lead(5, "p")];
+var classic_black_data = ["No name set", [0.967, 0.4, 1.05, 1], earth, new Ele_beat(1.81, 1, "e"), new Lead(-15, "f")];
+var classic_white_data = ["No name set", [1.027, 0.45, 0.87, 1], air, new Ele_beat(1.92, 1, "a"), new Lead(15, "a")];
+var classic_blue_data = ["No name set", [0.887, 0.4, 1.08, 1], water, new Ele_beat(2.1, 1, "w"), new Lead(-15, "w")];
 
+var black_falcon_data = ["No name set", [0.967, 0.25, 1.1, 1], water, new Ele_beat(2.2, 2, undefined), new Lead(-15, "a")];
+var dragon_knight_data = ["No name set", [1.127, 0.4, 0.86, 1], fire, new Ele_beat(2.1, 2, "f"), new Lead(-15, "f")];
+var dwarf_data = ["No name set", [1.087, 0.4, 0.82, 2], earth, new AOE(1.6, 2, "e"), new Lead(-15, "f")];
 
+var cedric_data = ["Cedric the Bull", [0.817, 0.4, 1.23, 1], fire, new Boost(1, 2, fire), new Lead(-30, "w")];
+var weezil_data = ["Weezil", [0.717, 0.25, 1.17, 1], fire, new Poison(2, 1), new Lead(10, "h")];
+var princess_storm_data = ["Princess Storm", [1.087, 0.4, 0.93, 1], fire, new Boost(1, 2, fire), new Lead(-30, "w")];
 
-var classic_red_data = ["No name set", [1.057, 0.4, 0.95, 1], fire, new Ele_beat(1.66, 1, "f")];
-var classic_black_data = ["No name set", [0.967, 0.4, 1.05, 1], earth, new Ele_beat(1.81, 1, "e")];
-var classic_white_data = ["No name set", [1.027, 0.45, 0.87, 1], air, new Ele_beat(1.92, 1, "a")];
-var classic_blue_data = ["No name set", [0.887, 0.4, 1.08, 1], water, new Ele_beat(2.1, 1, "w")];
+var gi_dan_data = ["Gi-Dan", [1.177, 0.4, 0.81, 2], water, new Ele_beat(1.87, 2, "w"), new Lead(10, "p")];
 
-var black_falcon_data = ["No name set", [0.967, 0.25, 1.1, 1], water, new Ele_beat(2.2, 2, undefined)];
-var dragon_knight_data = ["No name set", [1.127, 0.4, 0.86, 2], fire, new Ele_beat(2.1, 2, "f")];
-var dwarf_data = ["No name set", [1.087, 0.4, 0.82, 2], earth, new Ele_beat(2.2, 2, "e")];
+var black_knight_data = ["No name set", [1.107, 0.4, 1.12, 1], earth, new Ele_beat(1.67, 1, "e"), new Lead(5, "p")];
 
-var levahk_data = ["Levahk", [0.687, 0.5, 0.83, 1], air, new Ele_beat(3.95, 3, "a")];
-var nuhvok_data = ["Nuhvok", [0.687, 0.45, 1.04, 1], earth, new AOE(3.2, 3, "e")];
+var jayko_data = ["Jayko", [0.897, 0.25, 1.02, 1], water, new Ele_beat(1.95, 1, undefined), new Lead(5, "p")];
+var danju_data = ["Danju", [0.757, 0.25, 1.13, 1], air, new Healing(2.35, 3), new Lead(-30, "e")];
+var santis_data = ["Santis", [1.017, 0.25, 1.03, 1], fire, new Armor_break(1.99, 3), new Lead(10, "p")];
+var rascus_data = ["Rascus", [0.867, 0.25, 1.2, 1], earth, new Stealth_strike(2.11, 2), new Lead(30, "e")];
 
+var shadow_knight_data = ["No name set", [1.167, 0.4, 0.78, 1], earth, new Ele_beat(1.92, 2, "e"), new Lead(+5, "p")];
+var vladek_data = ["Vladek", [1.277, 0.45, 0.65, 1], earth, new Ele_beat(2.81, 4, "e"), new Lead(-30, "f")];
+var dracus_data = ["Dracus", [1.137, 0.3, 1.02, 1], air, new Boost(1, 2, air), new Lead(10, "p")];
+var karzon_data = ["Karzon", [0.937, 0.25, 1.12, 1], water, new Poison(1.89, 2), new Lead(-30, "w")];
 
-var first_war_vet = ["No name set", [1.175, 0.45, 0.88, 2], earth, new Ele_beat(1.8, 2, "e")];
-var saracen_data = ["No name set", [1.075, 0.33, 0.92, 0], fire, new Ele_beat(1.88, 1, "f")];
+var bonsai_data = ["Bonsai", [1.167, 0.1, 1.12, 0], earth, new Boost(1, 2, earth), new Lead(5, "p")];
+var kahuka_data = ["King Kahuka", [0.657, 0.2, 0.83, 0], air, new Poison(4, 4), new Lead(10, "h")];
 
+var levahk_data = ["Levahk", [0.687, 0.5, 0.83, 1], air, new Ele_beat(3.95, 2, "a"), new Lead(30, "a")];
+var nuhvok_data = ["Nuhvok", [0.687, 0.45, 1.04, 1], earth, new AOE(3.2, 3, "e"), new Lead(-30, "e")];
 
-
-var onua_data = ["Onua", [0.973, 0.2, 1.13, 1], earth, new Ele_beat(1.3, 2, "e")];
-
-
-// warriors
-var BART = new Warrior(bart_data, 34);
-
-
-
-var cred = new Warrior(classic_red_data, 34);
-var cblack = new Warrior(classic_black_data, 34);
-var cwhite = new Warrior(classic_white_data, 34);
-var cblue = new Warrior(classic_blue_data, 34);
-
-var bfalc = new Warrior(black_falcon_data, 34);
-var dknight = new Warrior(dragon_knight_data, 34);
-var dwarf = new Warrior(dwarf_data, 34);
-
-var levahk = new Warrior(levahk_data, 34);
-var nuhvok = new Warrior(nuhvok_data, 34);
+var kai_data = ["Kai", [1.317, 0.5, 0.65, 0], fire, new Ele_beat(1.72, 2, "f"), new Lead(30, "f")];
+var jay_data = ["Jay", [0.877, 0.5, 0.86, 0], water, new Rolling_thunder(2.4, 4), new Lead(-30, "a")];
+var cole_data = ["Cole", [0.967, 0.5, 0.87, 0], earth, new AOE(2.6, 4, "e"), new Lead(-30, "e")];
+var zane_data = ["Zane", [0.837, 0.5, 0.95, 0], water, new AOE(2.96, 4, "w"), new Lead(30, "w")];
 
 
-var fwv = new Warrior(first_war_vet, 34);
-var saracen = new Warrior(saracen_data, 34);
+// need to set these guys leader skills
+var first_war_vet = ["No name set", [1.175, 0.45, 0.88, 2], earth, new Ele_beat(1.8, 2, "e"), new Lead(5, "p")];
+var saracen_data = ["No name set", [1.075, 0.33, 0.92, 0], fire, new Ele_beat(1.88, 1, "f"), new Lead(5, "p")];
+var peter_data = ["Peter", [1.105, 0.66, 0.77, 1], water, new Ele_beat(2.5, 2, "w"), new Lead(-30, "w")];
+var lanze_data = ["Lanze", [0.905, 0.55, 1.01, 1], earth, new Ele_beat(3.25, 2, undefined), new Lead(-30, "f")];
+var anyo_data = ["Anyo", [1.115, 0.2, 0.9, 1], fire, new Poison(1.66, 2), new Lead(-30, "f")];
 
+var onua_data = ["Onua", [0.973, 0.2, 1.13, 1], earth, new Ele_beat(1.3, 2, "e"), new Lead(5, "p")];
+var lewa_data = ["Lewa", [0.983, 0.05, 1.2, 1], air, new Ele_beat(1.6, 1, "a"), new Lead(10, "h")];
 
+var tahu_u_data = ["Tahu Uniter", [1.123, 0.5, 1.12, 2], fire, new Ele_beat(2.1, 2, "f"), new Lead(30, "f")];
+var kopaka_u_data = ["Kopaka Uniter", [1.033, 0.25, 1.07, 1], water, new Ele_beat(1.8, 2, "w"), new Lead(10, "p")];
 
-var onua = new Warrior(onua_data, 34);
+var MNTS = new Team([new Warrior(levahk_data, 34), new Warrior(dragon_knight_data, 34), new Warrior(gi_dan_data, 34)]);
+var M_dual_boosts = new Team([new Warrior(karzon_data, 34), new Warrior(dracus_data, 34), new Warrior(princess_storm_data, 34)]);
+var morcia = new Team([new Warrior(danju_data, 34), new Warrior(santis_data, 34), new Warrior(rascus_data, 34)]);
+
+var NNTS = new Team([new Warrior(kopaka_u_data, 34), new Warrior(tahu_u_data, 34), new Warrior(lewa_data, 34)]);
+
+var paul_team = new Team([new Warrior(anyo_data, 34), new Warrior(lanze_data, 34), new Warrior(peter_data, 34)]);
+
+var team1 = morcia;
+var team2 = M_dual_boosts;
+b = new Battle(team1, team2);
+b.init();
