4daa0af9ba31fbaf6fa2bd280a15eae991914c90
IronHeart7334 <greengrappler12@gmail.com>
2017-08-30 07:23:38 -0700

    This is going to be huge

--- real_warriors.js
+++ real_warriors.js
@@ -4,20 +4,14 @@ var brutus = ["Brutus", [0.9334, 0.1, 0.8, 2], fire, new Leech_blade(2.05, 2), n
 var zazan = ["Zazan", [0.9, 0.25, 0.96, 1], fire, new Phantom_shield(), new Lead(15, "f")];
 var gunnar = ["Gunnar", [1.0665, 0.1, 0.78, 2], earth, new Berserk(2.05, 3), new Lead(15, "e")];
 var blaine = ["Blaine", [0.9167, 0.25, 0.75, 2], earth, new Regeneration(2.6, 2), new Lead(15, "e")];
-var toki = ["Toki", [1, 0.4, 1, 0], air, new Ele_beat(1.85, 2, "a"), new Lead(-15, "e")];
 var kendrix = ["Kendrix", [0.9334, 0.1, 1.1, 1], air, new Berserk(1.68, 2), new Lead(-15, "3")];
 var le_roc = ["Le Roc", [1.05, 0.25, 0.7, 2], air, new Ele_beat(2.15, 3, undefined), new Lead(15, "a")];
-// mod?
 var sakuma = ["Sakuma", [1.1665, 0.15, 0.75, 2], air, new Phantom_strike(1.4, 2), new Lead(5, "p")];
-var zenghis = ["Zenghis", [1.05, 0.4, 0.95, 1], water, new Ele_beat(1.55, 1, "w"), new Lead(-15, "a")];
-// mod?
 var clovis = ["Clovis", [0.8, 0.3, 0.91, 1], water, new Healing(3.1, 2), new Lead(-15, "w")];
 var yada = ["Yada", [0.7, 0.25, 1.1, 2], water, new Ele_beat(2.1, 1, undefined), new Lead(10, "h")];
 var ram = ["Ram", [0.8334, 0.25, 1.13, 0], water, new Poison(2.1, 1), new Lead(15, "w")];
 
 var naro = ["Naro", [0.766, 0.25, 0.6, 1], fire, new Ele_beat(3.5, 3, undefined), new Lead(25, "p")];
-var aka = ["Aka", [1.0165, 0.4, 0.9, 0], earth, new Ele_beat(2.2, 2, "e"), new Lead(5, "p")];
-var alkan = ["Alkan", [0.9332, 0.5, 0.7, 1], water, new Ele_beat(3.9, 3, "w"), new Lead(15, "w")];
 
 var breth = ["Breth", [1.1834, 0.25, 0.85, 1], water, new Phantom_shield(), new Lead(-30, "a")];
 
@@ -40,15 +34,134 @@ class Abu extends Warrior{
         super(["Abu", [0.9334, 0.4, 1.05, 1, 1], fire, new Beat(true), new Lead(15, "f")], level);
     }
 }
+class Kadam extends Warrior{
+
+}
+class Volten extends Warrior{
+
+}
+class Anil extends Warrior{
+
+}
+class Pacorus extends Warrior{
+
+}
+class Aka extends Warrior{
+	constructor(level){
+		super(["Aka", [1.0165, 0.4, 0.9, 0, 2], earth, new Beat(true), new Lead(5, "p")], level);
+	}
+}
+class Doongara extends Warrior{
+
+}
+class Seni extends Warrior{
+
+}
+class Toki extends Warrior{
+	constructor(level){
+		super(["Toki", [1, 0.4, 1, 0, 2], air, new Beat(true), new Lead(-15, "e")], level);
+	}
+}
+class Maximus extends Warrior{
+
+}
+class Viriathus extends Warrior{
+
+}
+class Zenghis extends Warrior{
+	constructor(level){
+		super(["Zenghis", [1.05, 0.4, 0.95, 1, 1], water, new Beat(true), new Lead(-15, "a")], level);
+	}
+}
+class Kasim extends Warrior{
+
+}
+class Alkan extends Warrior{
+	constructor(level){
+		super(["Alkan", [0.9332, 0.5, 0.7, 1, 3], water, new Beat(true), new Lead(15, "w")], level);
+	}
+}
+class GajahMada extends Warrior{
+
+}
+class Boris extends Warrior{
+	
+}
+class Hector extends Warrior{
+
+}
+class Gwen extends Warrior{
+
+}
 class Osgood extends Warrior{
     constructor(level){
         super(["Osgood", [0.7334, 0.4, 1.15, 1, 1], earth, new AOE(), new Lead(-15, "e")], level);
     }
 }
+class Ironhart extends Warrior{
+
+}
+class Zuu extends Warrior{
+
+}
+class Khutulune extends Warrior{
+
+}
+class Balash extends Warrior{
+
+}
+class Erika extends Warrior{
+
+}
+class Ping extends Warrior{
+
+}
+class Grim extends Warrior{
+
+}
+class Leon extends Warrior{
+
+}
+class Cutbert extends Warrior{
+
+}
+class Joan extends Warrior{
+
+}
+class Alboin extends Warrior{
+
+}
+class Tiberius extends Warrior{
+
+}
+class Mohinder extends Warrior{
+
+}
+class Badda extends Warrior{
+
+}
+class Spurius extends Warrior{
+
+}
 class Crixus extends Warrior{
     constructor(level){
         super(["Crixus", [0.8667, 0.4, 1.05, 2, 2], air, new Boost(air), new Lead(-15, "e")], level);
     }
+}
+class Labashi extends Warrior{
+
+}
+class Utu extends Warrior{
+
+}
+class Kwan extends Warrior{
+
+}
+class Simo extends Warrior{
+
+}
+class Manawa extends Warrior{
+
 }
 
 var abu1 = new Team([new Abu(5), new Osgood(5), new Crixus(5)], "ABU");
