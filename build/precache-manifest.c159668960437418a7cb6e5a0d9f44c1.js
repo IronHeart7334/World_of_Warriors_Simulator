f305ce87ca985c077951ffc2b7975cff1fe25aac
Matt Crow <matt@Aldens-MacBook-Air.local>
2019-05-07 13:54:27 -0700

    Using npm run build, then npm run start works now
    now let's see if I can put the real webpage in there...

---
+++ build/precache-manifest.c159668960437418a7cb6e5a0d9f44c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c601c6d4901169d21fde443d533d22e",
+    "url": "/IronHeart7334/World_of_Warriors_Simulator/index.html"
+  },
+  {
+    "revision": "cd864a88be4489431830",
+    "url": "/IronHeart7334/World_of_Warriors_Simulator/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "c9f12982f372f0147487",
+    "url": "/IronHeart7334/World_of_Warriors_Simulator/static/js/2.c6e4a416.chunk.js"
+  },
+  {
+    "revision": "cd864a88be4489431830",
+    "url": "/IronHeart7334/World_of_Warriors_Simulator/static/js/main.590ca446.chunk.js"
+  },
+  {
+    "revision": "a05973d0c421129e13ad",
+    "url": "/IronHeart7334/World_of_Warriors_Simulator/static/js/runtime~main.9d75d2e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/IronHeart7334/World_of_Warriors_Simulator/static/media/logo.5d5d9eef.svg"
+  }
+]);
