4d64a3ae03fa959ec1378c0132012e9f8bd6a514
IronHeart7334 <greengrappler12@gmail.com>
2019-08-18 14:30:53 -0700

    cleaned up most of battlePage, just a couple points I could clean up
    but I think I'll move on now: want to try and get this working by Saturday

--- src/gui/battlePage.js
+++ src/gui/battlePage.js
@@ -1,13 +1,11 @@
-import {GamePane} from "./gamePane.js";
-import {WarriorHud} from "./warriorHud.js";
-import {Button} from "./button.js";
+import {Canvas} from "./canvas.js";
 import {Stat} from "../warrior/stat.js";
 
 export class BattlePage{
     constructor(){
         this.team1 = null;
         this.team2 = null;
-        this.team1Turn = true;
+        this.currTeam = null; //current team whose turn it is
         this.inAttackPhase = false;
         this.repaintThese = []; //stuff to redraw
     }
@@ -63,65 +61,45 @@ export class BattlePage{
         });
         
         
-        this.team1Turn = Math.random() >= 0.5;
-        this.inAttackPhase = true; 
-        this.update();
+        this.currTeam = (Math.random() >= 0.5) ? this.team1 : this.team2;
+        this.attackPhaseFor(this.currTeam);
     }
     
     normalMove(){
         if(this.inAttackPhase){
-            if(this.team1Turn){
-                this.team1.active.useNormalMove();
-            }else{
-                this.team2.active.useNormalMove();
-            }
-            this.team1Turn = !this.team1Turn;
-            this.inAttackPhase = false;
-            this.update();
+            this.currTeam.active.useNormalMove();
+            this.currTeam = this.currTeam.enemyTeam;
+            this.recoverPhaseFor();
         }
     }
     
     specialMove(warrior){
         if(this.inAttackPhase){
-            //check if warrior is on the current team
-            if(
-                this.team1Turn && warrior.team === this.team1 || 
-                !this.team1Turn && warrior.team === this.team2
-            ){
+            if(this.currTeam === warrior.team){
                 warrior.useSpecial();
-                this.team1Turn = !this.team1Turn;
-                this.inAttackPhase = false;
-                this.update();
+                this.currTeam = this.currTeam.enemyTeam;
+                this.recoverPhaseFor();
             }
         }
     }
     
     heartColl(){
         if(!this.inAttackPhase){
-            if(this.team1Turn){
-                this.team1.active.nat_regen();
-                this.attackPhaseFor(this.team1);
-            } else {
-                this.team2.active.nat_regen();
-                this.attackPhaseFor(this.team2);
-            }
-            console.log("heal");
+            this.currTeam.active.nat_regen();
+            this.attackPhaseFor();
         }
     }
     
+    //move some of this to warrior
     bomb(){
         if(!this.inAttackPhase){
-            let team = (this.team1Turn) ? this.team1 : this.team2;
-            
-            let d = team.active.perc_hp(0.15);
-            team.active.hp_rem -= d;
-            if(team.active.hp_rem <= 1){
-                team.active.hp_rem = 1;
+            let d = this.currTeam.active.perc_hp(0.15);
+            this.currTeam.active.hp_rem -= d;
+            if(this.currTeam.active.hp_rem <= 1){
+                this.currTeam.active.hp_rem = 1;
             }
-            team.active.hp_rem = Math.round(team.active.hp_rem);
-            this.attackPhaseFor(team);
-            
-            console.log("boom!");
+            this.currTeam.active.hp_rem = Math.round(this.currTeam.active.hp_rem);
+            this.attackPhaseFor();
         }
     }
     
@@ -155,9 +133,9 @@ export class BattlePage{
             return;
         }
         
-        if(this.team1Turn && this.team1.energy >= 2){
+        if(this.currTeam === this.team1 && this.team1.energy >= 2){
             $(".t1-spec").show();
-        } else if(!this.team1Turn && this.team2.energy >= 2){
+        } else if(this.currTeam === this.team2 && this.team2.energy >= 2){
             $(".t2-spec").show();
         }
     }
@@ -172,45 +150,33 @@ export class BattlePage{
         * Armor: ${warrior.getStat(Stat.ARM)}\n`);
     }
     
-    recoverPhaseFor(team){
+    recoverPhaseFor(){
         this.inAttackPhase = false;
-        team.check_if_ko();
+        this.currTeam.check_if_ko();
         if(this.team1.won || this.team2.won){
             return;
         } //#################################STOPS HERE IF A TEAM WON
         
-        $("#vs-text").text(`${team.active.name} VS ${team.enemyTeam.active.name}`);
-        team.forEach((member)=>member.reset_heal());
+        $("#vs-text").text(`${this.currTeam.active.name} VS ${this.currTeam.enemyTeam.active.name}`);
+        this.currTeam.forEach((member)=>member.reset_heal());
         
         $(".recover").show();
         $(".attack").hide();
-        if (team.active.healableDamage <= 0){
+        if (this.currTeam.active.healableDamage <= 0){
             //no damage to heal, so skip this phase
-            this.attackPhaseFor(team); //recursive. Might not be good
+            this.attackPhaseFor();
         }
         this.draw();
     }
     
-    attackPhaseFor(team){
+    attackPhaseFor(){
         this.inAttackPhase = true;
         
-        team.turn_part2(); //lots of non-GUI stuff done here
+        this.currTeam.turn_part2(); //lots of non-GUI stuff done here
         $(".attack").show();
         $(".recover").hide();
         this.updateSpecials();
         
-        this.draw();
-    }
-    
-    update(){
-        if(this.team1Turn !== null){
-            let team = (this.team1Turn) ? this.team1 : this.team2;
-            if(this.inAttackPhase){
-                this.attackPhaseFor(team);
-            } else {
-                this.recoverPhaseFor(team);
-            }
-        }
         this.draw();
     }
     
@@ -219,5 +185,81 @@ export class BattlePage{
         this.repaintThese.forEach((element)=>{
             element.draw();
         });
+    }
+}
+
+
+class WarriorHud{
+    constructor(elementId, warrior){
+        this.warrior = warrior;
+        this.canvas = new Canvas(elementId);
+    }
+    
+    /*
+     * I want to redo this so that text looks better.
+     * Maybe incorperate multiple elements into the hud, 
+     * not just a canvas?
+     */
+    draw(){
+        let canvas = this.canvas;
+        canvas.setColor("black");
+        canvas.rect(0, 0, 100, 100); //clear the canvas
+        
+        if(this.warrior.check_if_ko()){
+            return;
+        }//########################################## STOPS HERE IF KOED
+        
+        //'active' border
+        if(this.warrior.team.active === this.warrior){
+            canvas.setColor("grey");
+            canvas.rect(0, 0, 100, 100);
+        }
+        
+        //boost
+        if(this.warrior.boostIsUp){
+            canvas.setColor(this.warrior.element.color);
+            canvas.rect(0, 0, 100, 100 / 2);
+        }
+        
+        //health bar
+        let color;
+        //I think I've just been poisoned...
+        //ergh this is awful
+        if(this.warrior.poisoned !== false){
+            color = "green";
+        } else {
+            color = "red";
+        }
+        canvas.setColor(color);
+        canvas.rect(50, 0, 50 * this.warrior.hp_perc(), 50);
+        
+        // health value
+        canvas.text(50, 0, this.warrior.name);
+        if (this.warrior.regen){
+            canvas.text(50, 50, this.warrior.hp_rem + "+");
+        } else {
+            canvas.text(50, 50, this.warrior.hp_rem);
+        }
+        
+        if (this.warrior.lastPhysDmg !== 0){
+            canvas.text(50, 50, "-" + String(Math.round(this.warrior.lastPhysDmg)));
+        }
+        if (this.warrior.lastEleDmg !== 0){
+            //make this text colored
+            canvas.text(50, 50, "-" + String(Math.round(this.warrior.lastEleDmg)));
+        }
+        if (this.warrior.last_healed !== 0){
+            canvas.text(50, 50, "+" + String(this.warrior.last_healed));
+        }
+
+        // Phantom Shield overlay
+        if (this.warrior.shield){
+            canvas.setColor("rgba(0, 0, 155, 0.5)");
+            canvas.rect(0, 0, 100, 100 / 2);
+        }
+
+        // icon
+        canvas.setColor(this.warrior.element.color);
+        canvas.circle(0, 0, 50);
     }
 }
