ba8939b8e1f65dc0f063fe8affdbf2032b5f056d
IronHeart7334 <greengrappler12@gmail.com>
2020-03-28 13:41:52 -0700

    I forgot how much fun JS was. Want to get started on WoW module standards.

--- src/app.js
+++ src/app.js
@@ -6,21 +6,27 @@ import {Warrior} from "./warrior/warrior.js";
 
 import {CriticalHit, Guard} from "./warrior/warriorSkills.js";
 
+import {getModuleList} from "./util/import.js";
+
 let user = new User();
 user.warriors = warriors;
 user.teams = [
     new Team("Starter Team", [
-        new Warrior("Abu"), 
-        new Warrior("Toki"), 
+        new Warrior("Abu"),
+        new Warrior("Toki"),
         new Warrior("Zenghis")
     ]),
     new Team("Arena Favorites", [
-        new Warrior("Ironhart"), 
-        new Warrior("Erika"), 
+        new Warrior("Ironhart"),
+        new Warrior("Erika"),
         new Warrior("Boris")
     ])
 ];
 user.loadModules();
+getModuleList().then((list)=>{
+    console.log(list);
+    //need to use then, as 'await' doesn't work, given that this isn't wrapped in an async function
+});
 //user.teams[0].members[0].addSkill(new CriticalHit());
 //user.teams[0].members[1].addSkill(new Guard());
 
---
+++ src/util/import.js
@@ -0,0 +1,44 @@
+const MODULE_PATH_BASE = "./data";
+const MODULE_LIST_PATH = MODULE_PATH_BASE + "/installedModules.txt";
+const NEWLINE = /\r?\n|\r/;
+
+//next, work on explicitly defining the standards for WoW modules.
+
+async function getModuleList(){
+    let req = await fetch(MODULE_LIST_PATH);
+    let text = await req.text();
+    let moduleList = [];
+    text.split(NEWLINE).forEach((line)=>{
+        if(line.trim().length > 0 && line[0] !== "#"){
+            moduleList.push(line.trim());
+        }
+    });
+    return moduleList;
+}
+
+async function loadModule(moduleName){
+    let req = await fetch(MODULE_PATH_BASE + "/" + moduleName);
+    let text = await req.text();
+
+    //this is the format warriors are stored in in wowdata.csv,
+    //but I'm not sure if I want to save it in a different way,
+    //probably not though.
+    /*
+    let columns = [
+        "Warrior",
+        "Rank",
+       	"Commonality",
+        "Skill 1",
+        "Skill 2",
+        "Base Phys",
+        "Base Ele",
+        "Base HP",
+        "Armor",
+       	"Leader Skill",
+        "Special"
+    ];*/
+}
+
+export {
+    getModuleList
+};
